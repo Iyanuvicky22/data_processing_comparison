c023ec73e2a30c65c208b70a41c5a5b25ec202d8
Arowosegbe Victor <iyanuvicky@outlook.com>
2026-03-08 10:25:00 +0100

    Modified the polars outlier handling function using polars based code instead of the previous method of first converting to pandas, handling outliers and reconverting to polars

--- processor/clean.py
+++ processor/clean.py
@@ -95,60 +95,76 @@ def handle_outlier_pandas(df: pd.DataFrame, col: str, method="cap"):
         logger.error("Dataframe not of pandas type")
 
 
-def handle_outlier_polars(df: pl.DataFrame, col: str, method="cap"):
+def handle_outlier_polars(df: pl.DataFrame, column: str, method="cap"):
     """
     This function handles the
     identified outliers in the polars dataframe
     Args:
         df (pd.DataFrame): Pandas Dataframe
-        col (str): Column with outliers
+        column (str): Column with outliers
         method (str, optional): Method to use in handling outliers.
         Defaults to 'cap'.
     """
-    df = df.to_pandas()
-    q1 = df[col].quantile(0.25)
-    q3 = df[col].quantile(0.75)
-
-    iqr = q3 - q1
-
     try:
         if method == "drop":
-            df[col] = df[col][
-                ~((df[col] < (q1 - 1.5 * iqr)) | (df[col] > (q3 + 1.5 * iqr)))
-            ]
-            df = df.dropna()
-            df = pl.from_pandas(df)
+            df.with_columns(
+                q1_drop=pl.col(column).quantile(0.25),
+                q3_drop=pl.col(column).quantile(0.75)
+            ).with_columns(
+                iqr_drop=pl.col('q3_drop') - pl.col('q1_drop')
+            ).with_columns(
+                lower=pl.col('q1_drop') - 1.5 * pl.col('iqr_drop'),
+                upper=pl.col('q3_drop') + 1.5 * pl.col('iqr_drop')
+            ).filter(
+                (pl.col(column) >= pl.col('lower')) 
+                &
+                (pl.col(column) <= pl.col('upper'))
+            ).drop(['q1_drop', 'q3_drop', 'iqr_drop', 'lower', 'upper'])
             return df
         if method == "cap":
-            upper_limit = df[col].mean() + 3 * df[col].std()
-            lower_limit = df[col].mean() - 3 * df[col].std()
-            df[col] = df[col].astype(float)
-            df[col] = np.where(
-                df[col] > upper_limit,
-                upper_limit,
-                np.where(df[col] < lower_limit, lower_limit, df[col]),
+            upper = pl.col(column).mean() + 3 * pl.col(column).std().cast(pl.Int64)
+            lower = pl.col(column).mean() - 3 * pl.col(column).std().cast(pl.Int64)
+
+            df.with_columns(
+                pl.when(pl.col(column) > upper)
+                .then(upper)
+                .when(pl.col(column) < lower)
+                .then(lower)
+                .otherwise(pl.col(column))
+                .cast(pl.Int64)
+                .alias(column),
             )
-            df[col] = df[col].astype(int)
-            df = pl.from_pandas(df)
             return df
         if method == "mean":
-            lower_limit = df[col][~(df[col] < (q1 - 1.5 * iqr))].max()
-            upper_limit = df[col][~(df[col] > (q3 + 1.5 * iqr))].min()
-            df[col] = df[col].astype(float)
-            df[col] = np.where(
-                df[col] > upper_limit,
-                df[col].mean(),
-                np.where(df[col] < lower_limit, df[col].mean(), df[col]),
+            q1 = df.select(pl.col(column).quantile(0.25)).item()
+            q3 = df.select(pl.col(column).quantile(0.75)).item()
+            iqr = q3 - q1
+            lower_bound = q1 - 1.5 * iqr
+            upper_bound = q1 + 1.5 * iqr
+            lower_limit = df.filter(
+                pl.col(column) >= lower_bound
+            ).select(pl.col(column).min()).item()
+
+            upper_limit = df.filter(
+                pl.col(column) <= upper_bound
+            ).select(pl.col(column).max()).item()
+
+            median_val = df.select(pl.col(column).mean()).item()
+
+            df.with_columns(
+                pl.when(pl.col(column) > upper_limit)
+                .then(median_val)
+                .when(pl.col(column) < lower_limit)
+                .then(median_val)
+                .otherwise(pl.col(column))
+                .cast(pl.Int64)
+                .alias(column)
             )
-            df[col] = df[col].astype(int)
-            df = pl.from_pandas(df)
             return df
     except NameError:
-        logger.error("Column not in dataframe columns.")
+        print("Column not in dataframe columns.")
     except AttributeError:
-        logger.error("Dataframe not of polars type")
-    except Exception as e:
-        logger.error("Error Encountered", e)
+        print("Dataframe not of polars type")
 
 
 def transform_df(df: pd.DataFrame | pl.DataFrame) -> pd.DataFrame | pl.DataFrame:
